aec26f8b188693c46af621af6401217b058cecb8
dennispradana <dennisoldyatha@gmail.com>
2022-11-20 18:40:59 +0700

    fix structure file and folder

---
+++ assets/script/dataTabel.js
@@ -0,0 +1,32 @@
+$(function () {
+  $("#kontrakKerja").DataTable({
+    paging: true,
+    lengthChange: false,
+    searching: false,
+    ordering: true,
+    info: true,
+    autoWidth: false,
+  });
+});
+
+$(function () {
+  $("#penyedia").DataTable({
+    paging: true,
+    lengthChange: false,
+    searching: false,
+    ordering: true,
+    info: true,
+    autoWidth: false,
+  });
+});
+
+$(function () {
+  $("#satuanKerja").DataTable({
+    paging: true,
+    lengthChange: false,
+    searching: false,
+    ordering: true,
+    info: true,
+    autoWidth: false,
+  });
+});
---
+++ assets/script/datePicker.js
@@ -0,0 +1,35 @@
+$("#perencanaan").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
+
+$("#pengadaan").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
+
+$("#kontrak").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
+
+$("#pekerjaanAwal").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
+
+$("#akta").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
+
+$("#tanggalAkta").datepicker({
+  format: "dd/mm/yyyy",
+  autoclose: true,
+  todayHighlight: true,
+});
